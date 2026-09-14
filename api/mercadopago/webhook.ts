787a2b1bcc286e1f6ff8d77aa91c1e0a046f20bd
Giovanne <giovannecoelho@gmail.com>
2026-09-14 10:29:37 -0300

    fix(api): remover extensoes .ts em imports de rotas api para garantir compilacao Vercel Serverless

--- api/mercadopago/webhook.ts
+++ api/mercadopago/webhook.ts
@@ -1,4 +1,4 @@
 // Vercel Serverless Function Alias for Mercado Pago Webhook (/api/mercadopago/webhook)
-import handler from "../webhooks/mercadopago.ts";
+import handler from "../webhooks/mercadopago";
 
 export default handler;
--- api/webhook-mp.ts
+++ api/webhook-mp.ts
@@ -1,4 +1,4 @@
 // Vercel Serverless Function Alias for Mercado Pago Webhook (/api/webhook-mp)
-import handler from "./webhooks/mercadopago.ts";
+import handler from "./webhooks/mercadopago";
 
 export default handler;
